d4d73e36118f622615d2d3c459e891650dcbefa9
frantene <112424233+frantene@users.noreply.github.com>
2023-12-18 14:04:53 -0600

    Update hello.php

--- hello.php
+++ hello.php
@@ -1,9 +1,19 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>PHP Test</title>
-    </head>
-    <body>
-        <?php echo '<p>Hello World</p>'; ?>
-    </body>
-</html>
+<a href="#" class="MyLink"><img src="./yourImageIcon.png" /><span class="MyLinkText">SIGN IN</span></a>
+<style>
+    .linkWithImageIcon{
+
+    Display:inline-block;
+}
+.MyLink{
+  Background:#FF3300;
+  width:200px;
+  height:70px;
+  vertical-align:top;
+  display:inline-block; font-weight:bold;
+} 
+
+.MyLinkText{
+  /*---The margin depends on how the image size is ---*/
+  display:inline-block; margin-top:5px;
+}
+</style>
